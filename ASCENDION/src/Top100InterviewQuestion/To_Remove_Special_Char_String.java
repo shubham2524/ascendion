e68cd36cbc176291e070be735d3c05411119eb7b
shubham.saurabh <shubham.saurabh@ASCINLAP30343.bbrouter>
2024-05-06 20:51:25 +0530

    commit

--- ASCENDION/src/Top100InterviewQuestion/To_Remove_Special_Char_String.java
+++ ASCENDION/src/Top100InterviewQuestion/To_Remove_Special_Char_String.java
@@ -3,7 +3,7 @@ package Top100InterviewQuestion;
 public class To_Remove_Special_Char_String {
 
 	public static void main(String[] args) {
-		String s = "shubham@#$%^&";
+		String s = "shubham@#^$%^&";
 		String sN = s.replaceAll("[^a-zA-Z0-9]","");
 		System.out.println(sN);
 	}
